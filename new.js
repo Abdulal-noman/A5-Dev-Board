7604987a8df62075fdb6f1be5c9aa1cd4ca7e774
Abdulal-noman <abdulalnomanforcse@gamil.com>
2025-03-02 01:25:31 +0600

    first commit

---
+++ new.js
@@ -0,0 +1,15 @@
+
+// new page come
+document.getElementById("next-btn").addEventListener("click",function(){
+    window.location.href="new.html"
+    
+});
+
+// index page back
+document.getElementById("back-btn").addEventListener("click", function() {
+    console.log("Back button clicked");
+    // window.location.href = "index.html";
+});
+
+
+
